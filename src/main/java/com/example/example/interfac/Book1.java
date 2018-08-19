416a4e010add290074676511121bca06509c516a
Rineethanjs <rineethanjs@gmail.com>
2018-08-18 23:47:49 -0500

    Committing Arrays

---
+++ src/main/java/com/example/example/interfac/Book1.java
@@ -0,0 +1,15 @@
+package com.example.example.interfac;
+import java.util.*;  
+	class Book1 
+	{  
+	int id;  
+	String name,author,publisher;  
+	int quantity;  
+	public Book1(int id, String name, String author, String publisher, int quantity) {  
+    this.id = id;  
+	    this.name = name;  
+	    this.author = author;  
+	    this.publisher = publisher;  
+	    this.quantity = quantity;  
+	}  
+	}  
